f88a0be953cb3757e2b3379dd389caa57d336019
Lubomir Drab <lubo.drab@gmail.com>
2023-01-16 13:48:58 +0100

    New project created

---
+++ pay-gate/src/main/java/com/gfa/paygate/PayGateApplication.java
@@ -0,0 +1,13 @@
+package com.gfa.paygate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayGateApplication.class, args);
+	}
+
+}
---
+++ pay-gate/src/test/java/com/gfa/paygate/PayGateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfa.paygate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayGateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
